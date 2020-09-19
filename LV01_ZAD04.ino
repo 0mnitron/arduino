5357ec9a75415bc17cc2f3d9b1b6d7e5fbaf4a57
0mnitron <56760837+0mnitron@users.noreply.github.com>
2020-09-19 17:57:16 +0200

    Create LV01_ZAD04.ino

---
+++ LV01_ZAD04.ino
@@ -0,0 +1,29 @@
+int led1 = 9;         //crvena
+int led2 = 10;        //žuta
+int led3 = 11;        //zelena
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led1, HIGH);
+  delay(10000);
+  
+  digitalWrite(led2, HIGH);
+  delay(2000);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+  digitalWrite(led3, HIGH);
+  delay(10000);
+  
+  digitalWrite(led3, LOW);
+  digitalWrite(led2, HIGH);
+  delay(2000);
+  
+  digitalWrite(led2, LOW);
+  }
